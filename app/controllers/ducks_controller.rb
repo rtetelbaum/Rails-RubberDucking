0d966bf0d819a59125c38d34dfd91bcfe777ba98
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-14 21:13:30 -0400

    CC done

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,48 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+	def show
+		find_duck
+	end
+
+	def new
+		@duck = Duck.new
+		@students = Student.all
+	end
+
+	def create
+		@duck = Duck.new(duck_params)
+		if @duck.save
+			redirect_to duck_path(@duck)
+		else
+			flash[:errors] = @duck.errors.full_messages
+			redirect_to new_duck_path
+		end
+	end
+
+	def edit
+		find_duck
+		@students = Student.all
+	end
+
+	def update
+		find_duck
+		if @duck.update(duck_params)
+			redirect_to duck_path(@duck)
+		else
+			flash[:errors] = @duck.errors.full_messages
+			redirect_to edit_duck_path
+		end
+	end
+
+	private
+
+	def duck_params
+		params.require(:duck).permit(:name, :description, :student_id)
+	end
+
+	def find_duck
+		@duck = Duck.find(params[:id])
+	end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,44 @@ class StudentsController < ApplicationController
 	end
 	
 	def show
+		find_student
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+		@student = Student.new(student_params)
+		if @student.save
+			redirect_to student_path(@student)
+		else
+			flash[:errors] = @student.errors.full_messages
+			redirect_to new_student_path
+		end
+	end
+
+	def edit
+		find_student
+	end
+
+	def update
+		find_student
+		if @student.update(student_params)
+			redirect_to student_path(@student)
+		else
+			flash[:errors] = @student.errors.full_messages
+			redirect_to edit_student_path
+		end
+	end
+
+	private
+
+	def student_params
+		params.require(:student).permit(:name, :mod)
+	end
+
+	def find_student
 		@student = Student.find(params[:id])
 	end
 
